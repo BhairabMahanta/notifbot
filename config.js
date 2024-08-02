48216f288fa18e48b756fd5dd191870a588af7af
Akaimnky <bhairabm1908@gmail.com>
2024-08-03 03:25:32 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  token: "your discord bot token",
+  channel: "your discord channelId",
+  messageTemplate:
+    "Hello everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+  //dont change messageTemplate
+  channel_id: "advanced settings (in pc) -> channel id",
+  watchInterval: 10000,
+  youtubeApiKey: "Token from google cloud platform",
+};
---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const axios = require("axios");
+const config = require("./config");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+let lastVideoId = null;
+
+async function checkYouTube() {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${config.youtubeApiKey}&channelId=${config.channel_id}&part=snippet,id&order=date&maxResults=1`
+    );
+
+    const video = response.data.items[0];
+
+    console.log("Checking for new video:", video);
+    if (video.id.videoId !== lastVideoId) {
+      lastVideoId = video.id.videoId;
+      const channel = await client.channels.fetch(config.channel);
+      const message = config.messageTemplate
+        .replace("{author}", video.snippet.channelTitle)
+        .replace("{title}", video.snippet.title)
+        .replace(
+          "{url}",
+          `https://www.youtube.com/watch?v=${video.id.videoId}`
+        );
+      channel.send(message);
+    }
+  } catch (error) {
+    console.error("Error fetching YouTube data:", error);
+  }
+}
+
+client.once("ready", () => {
+  console.log("Bot is online!");
+  checkYouTube();
+  setInterval(checkYouTube, config.watchInterval);
+});
+
+client.login(config.token);
